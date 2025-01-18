cc303b8c59143603264949c628c3866eeb2ad8c6
soulcatdev <wangyehchernziyang@gmail.com>
2025-01-18 13:30:47 +0800

    Update DoorController.cs

--- DoorController.cs
+++ DoorController.cs
@@ -1,173 +1,49 @@
-using System.Collections;
-using ControlFreak2;
-using Photon.Pun;
+
+
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DoorController : MonoBehaviour
 {
-	private Door lastDoor;
+    private Animator anim; // 门的动画组件
+    public Transform emptyObject1; // 第一个空对象
+    public Transform emptyObject2; // 第二个空对象
+    public Transform player;       // 玩家
+    public float interactionRadius = 3f; // 半径
 
-	private Gate lastGate;
+    public AudioSource openDoorAudio; // 开门音效
+    public AudioSource closeDoorAudio; // 关门音效
 
-	private Elevator lastElev;
+    void Start()
+    {
+        anim = GetComponent<Animator>(); // 获取Animator组件
+    }
 
-	private SCP1162 last1162;
+    void Update()
+    {
+        // 计算玩家与门的距离
+        float distance = Vector3.Distance(player.position, transform.position);
 
-	private SCP106ControlPanel scp106;
-
-	private AlphaWarhead lastAlpha;
-
-	private GameObject uiButton;
-
-	private GameObject needKeycard;
-
-	private WaitForSeconds delay = new WaitForSeconds(1.5f);
-
-	private void Start()
-	{
-		if (((Component)this).gameObject.GetComponent<PhotonView>().IsMine)
-		{
-			uiButton = GameObject.Find("DoorButton");
-			needKeycard = GameObject.Find("NeedKeycard");
-			if ((Object)(object)uiButton != (Object)null)
-			{
-				uiButton.SetActive(false);
-			}
-			if ((Object)(object)needKeycard != (Object)null)
-			{
-				needKeycard.SetActive(false);
-			}
-		}
-	}
-
-	private void Update()
-	{
-		if (((Component)this).gameObject.GetComponent<PhotonView>().IsMine)
-		{
-			if ((Object)(object)lastDoor != (Object)null && CF2Input.GetButtonDown("Interact"))
-			{
-				lastDoor.ChangeState();
-				uiButton.SetActive(false);
-			}
-			if ((Object)(object)lastGate != (Object)null && CF2Input.GetButtonDown("Interact"))
-			{
-				lastGate.ChangeState();
-				uiButton.SetActive(false);
-			}
-			if ((Object)(object)scp106 != (Object)null && CF2Input.GetButtonDown("Interact"))
-			{
-				scp106.Contain();
-				uiButton.SetActive(false);
-			}
-			if (Object.op_Implicit((Object)(object)lastElev) && CF2Input.GetButtonDown("Interact"))
-			{
-				((Component)lastElev).GetComponent<PhotonView>().RPC("Use", (RpcTarget)0, new object[0]);
-				uiButton.SetActive(false);
-			}
-			if (Object.op_Implicit((Object)(object)last1162) && CF2Input.GetButtonDown("Interact"))
-			{
-				((Component)last1162).GetComponent<PhotonView>().RPC("GetRandomItem", (RpcTarget)0, new object[0]);
-				uiButton.SetActive(false);
-			}
-			if (Object.op_Implicit((Object)(object)lastAlpha) && CF2Input.GetButtonDown("Interact"))
-			{
-				((Component)lastAlpha).GetComponent<PhotonView>().RPC("Detonate", (RpcTarget)0, new object[0]);
-				uiButton.SetActive(false);
-			}
-		}
-	}
-
-	public void Entered(Door _lastDoor, Gate _lastGate, SCP106ControlPanel _scp106, Elevator _elev, SCP1162 _1162, AlphaWarhead _alpha)
-	{
-		if (!((Component)this).gameObject.GetComponent<PhotonView>().IsMine)
-		{
-			return;
-		}
-		if ((Object)(object)uiButton == (Object)null)
-		{
-			uiButton = GameObject.Find("DoorButton");
-		}
-		if ((Object)(object)_lastDoor != (Object)null)
-		{
-			lastDoor = _lastDoor;
-			if ((Object)(object)uiButton != (Object)null)
-			{
-				uiButton.SetActive(true);
-			}
-			return;
-		}
-		if ((Object)(object)_lastGate != (Object)null)
-		{
-			lastGate = _lastGate;
-			if ((Object)(object)uiButton != (Object)null)
-			{
-				uiButton.SetActive(true);
-			}
-			return;
-		}
-		if ((Object)(object)_scp106 != (Object)null)
-		{
-			scp106 = _scp106;
-			if ((Object)(object)uiButton != (Object)null)
-			{
-				uiButton.SetActive(true);
-			}
-			return;
-		}
-		if ((Object)(object)_elev != (Object)null)
-		{
-			lastElev = _elev;
-			if ((Object)(object)uiButton != (Object)null)
-			{
-				uiButton.SetActive(true);
-			}
-			return;
-		}
-		if ((Object)(object)_1162 != (Object)null)
-		{
-			last1162 = _1162;
-			if ((Object)(object)uiButton != (Object)null)
-			{
-				uiButton.SetActive(true);
-			}
-			return;
-		}
-		if ((Object)(object)_alpha != (Object)null)
-		{
-			lastAlpha = _alpha;
-			if ((Object)(object)uiButton != (Object)null)
-			{
-				uiButton.SetActive(true);
-			}
-			return;
-		}
-		lastDoor = null;
-		lastGate = null;
-		scp106 = null;
-		lastElev = null;
-		last1162 = null;
-		lastAlpha = null;
-		if ((Object)(object)uiButton != (Object)null)
-		{
-			uiButton.SetActive(false);
-		}
-	}
-
-	public void NeedKeycard(int id)
-	{
-		if (((Component)this).gameObject.GetComponent<PhotonView>().IsMine)
-		{
-			((MonoBehaviour)this).StartCoroutine(DisplayNeedKeycard(id));
-		}
-	}
-
-	private IEnumerator DisplayNeedKeycard(int id)
-	{
-		needKeycard.SetActive(true);
-		needKeycard.GetComponent<Text>().text = "Need keycard level " + id;
-		yield return delay;
-		needKeycard.GetComponent<Text>().text = string.Empty;
-		needKeycard.SetActive(false);
-	}
+        // 如果玩家在半径范围内且按下E键
+        if (distance <= interactionRadius && Input.GetKeyDown(KeyCode.E))
+        {
+            // 判断两个空对象的坐标是否相同
+            if (emptyObject1.position == emptyObject2.position)
+            {
+                anim.SetTrigger("Open"); // 播放开门动画
+                if (openDoorAudio != null) // 检查是否设置了开门音效
+                {
+                    openDoorAudio.Play(); // 播放开门音效
+                }
+            }
+            else
+            {
+                anim.SetTrigger("Closed"); // 播放关门动画
+                if (closeDoorAudio != null) // 检查是否设置了关门音效
+                {
+                    closeDoorAudio.Play(); // 播放关门音效
+                }
+            }
+        }
+    }
 }
+
